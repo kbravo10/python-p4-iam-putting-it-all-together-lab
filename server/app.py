6f273165877d1ecb0846e3b6a81d2a7d176ba65f
Kevin Bravo <kbravo1024@gmail.com>
2023-09-17 17:34:26 -0700

    complete iam lab

--- server/app.py
+++ server/app.py
@@ -23,44 +23,66 @@ class Signup(Resource):
                 image_url = json_image
             )
             new_user.password_hash = json['password']
-            # new_user_dict = new_user.to_dict()
+            new_user_dict = new_user.to_dict()
+            print(type(new_user_dict))
             db.session.add(new_user)
             db.session.commit()
             session['user_id'] = new_user.id
-            return new_user.to_dict(), 201
+            return new_user_dict,201
         else:
             return {"message": "Unprocessable Entity"}, 422 
 
 class CheckSession(Resource):
     def get(self):
+        print(session['user_id'])
         if session['user_id']:
-            user = User.query.filter(User.id == session['user_id']).first().to_dict()
-            return user, 200
+            user_id = session['user_id']
+            user = User.query.filter(User.id == user_id).first()
+            user_info = {
+                'username':user.username,
+                'id':user.id,
+                'image_url':user.image_url,
+                'bio':user.bio
+            }
+            return user_info, 200
         else:
+            print('no luck')
             return {"message":"Unathorized"}, 401
 
 class Login(Resource):
     def post(self):
-        user = request.get_json()
-        username = User.query.filter(User.username == user['username']).first()
-        if username:
-            if username.authenticate(user['password']) == True:
-                session['user_id'] = username.id
-                return username.to_dict(), 200
+        user = request.get_json()           
+        user_name = User.query.filter(User.username == user["username"]).first()
+        if user_name:
+            if user_name.authenticate(user['password']) == True:
+                user_info = {
+                    'username':user_name.username,
+                    'id':user_name.id,
+                    'image_url':user_name.image_url,
+                    'bio':user_name.bio
+                }
+                session['user_id'] = user_name.id
+                print(user_info)
+                return user_info, 200
+            else:
+                return {"message":"Unathorized"}, 401
         else:
             return {"message":"Unathorized"}, 401
 
 class Logout(Resource):
     def delete(post):
         if session['user_id']:
+            print(session['user_id'])
             session['user_id'] = None
             return {"message":"No Content"}, 204
         return {"message":"Unathorized"}, 401
 
 class RecipeIndex(Resource):
     def get(self):
+        print(session['user_id'])
         if session['user_id']:
             recipes_user = Recipe.query.filter(Recipe.user_id == session['user_id']).all()
+            print(f'{len(recipes_user)} doneeeeee')
             recipes = []
             for r in recipes_user:
                 recipe_dict = {
@@ -73,6 +95,7 @@ class RecipeIndex(Resource):
         return {'message':'Unathorized'}, 401
     
     def post(post):
+        print(session['user_id'])
         if session['user_id']:
             data = request.get_json()
             if len(data['instructions']) >= 50:
@@ -87,6 +110,7 @@ class RecipeIndex(Resource):
                 new_recipe_dict = new_recipe.to_dict()
                 db.session.add(new_recipe)
                 db.session.commit()
+                print('added recepe')
                 return new_recipe_dict, 201
             return {"message":"Unprocessable Entity"}, 422
         return {'message':'Unathorized'}, 401
