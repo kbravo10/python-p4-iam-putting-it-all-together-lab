1ff53c4bfc34b87d3f6f7e7237462352942d73ee
Kevin Bravo <kbravo1024@gmail.com>
2023-09-20 19:01:36 -0700

    complete lab IAM

--- server/app.py
+++ server/app.py
@@ -11,24 +11,27 @@ class Signup(Resource):
     def post(self):
         json = request.get_json()
         if 'username' in json and 'password' in json:
-            if len(json['username']) != 0: 
-                json_bio = None
-                json_image = None
-                if 'bio' in json:
-                    json_bio = json['bio']
-                if 'image_url' in json:
-                    json_image = json['image_url']
-                new_user = User(
-                    username=json['username'],
-                    bio =json_bio,
-                    image_url = json_image
-                )
-                new_user.password_hash = json['password']
-                new_user_dict = new_user.to_dict()
-                db.session.add(new_user)
-                db.session.commit()
-                session['user_id'] = new_user.id
-                return new_user_dict,201
+            try:
+                if len(json['username']) != 0: 
+                    json_bio = None
+                    json_image = None
+                    if 'bio' in json:
+                        json_bio = json['bio']
+                    if 'image_url' in json:
+                        json_image = json['image_url']
+                    new_user = User(
+                        username=json['username'],
+                        bio =json_bio,
+                        image_url = json_image
+                    )
+                    new_user.password_hash = json['password']
+                    new_user_dict = new_user.to_dict()
+                    db.session.add(new_user)
+                    db.session.commit()
+                    session['user_id'] = new_user.id
+                    return new_user_dict,201
+            except Exception:
+                return {"errors": ["Unprocessable Entity"]}, 422
             else:
                 return {"errors": ["Unprocessable Entity"]}, 422
         else:
@@ -48,45 +51,25 @@ class CheckSession(Resource):
                 return user_info, 200
         else:
             return {"errors":["Unathorized"]}, 401
-        
-@app.route("/login", methods=['POST'])
-def login():
-    user = request.get_json()           
-    user_name = User.query.filter(User.username == user["username"]).first()
-    if user_name != None:
-        if user_name.authenticate(user['password']) == True:
-            user_info = {
-                'username':user_name.username,
-                'id':user_name.id,
-                'image_url':user_name.image_url,
-                'bio':user_name.bio
-            }
-            session['user_id'] = user_name.id
-            return user_info, 200
+class Login(Resource):
+    def post(self):
+        user = request.get_json()           
+        user_name = User.query.filter(User.username == user["username"]).first()
+        if user_name != None:
+            if user_name.authenticate(user['password']) == True:
+                user_info = {
+                    'username':user_name.username,
+                    'id':user_name.id,
+                    'image_url':user_name.image_url,
+                    'bio':user_name.bio
+                }
+                session['user_id'] = user_name.id
+                return user_info, 200
+            else:
+                return {"errors":["Unathorized"]}, 401
         else:
-            return {"errors":["Unathorized"]}, 401
-    else:
-            
-        return {"errors": ["Unathorized"]}, 401
-# class Login(Resource):
-#     def post(self):
-#         user = request.get_json()           
-#         user_name = User.query.filter(User.username == user["username"]).first()
-#         if user_name != None:
-#             if user_name.authenticate(user['password']) == True:
-#                 user_info = {
-#                     'username':user_name.username,
-#                     'id':user_name.id,
-#                     'image_url':user_name.image_url,
-#                     'bio':user_name.bio
-#                 }
-#                 session['user_id'] = user_name.id
-#                 return user_info, 200
-#             else:
-#                 return {"errors":["Unathorized"]}, 401
-#         else:
              
-#             return {"errors": ["Unathorized"]}, 401
+            return {"errors": ["Unathorized"]}, 401
 class Logout(Resource):
     def delete(post):
         if session['user_id']:
@@ -137,7 +120,7 @@ class RecipeIndex(Resource):
 
 api.add_resource(Signup, '/signup', endpoint='signup')
 api.add_resource(CheckSession, '/check_session', endpoint='check_session')
-# api.add_resource(Login, '/login', endpoint='login')
+api.add_resource(Login, '/login', endpoint='login')
 api.add_resource(Logout, '/logout', endpoint='logout')
 api.add_resource(RecipeIndex, '/recipes', endpoint='recipes')
 
